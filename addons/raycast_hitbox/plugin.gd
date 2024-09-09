27a1d88a4417df28042dd3851d7304295aab7443
Tyler <itsteccs@gmail.com>
2024-09-08 20:12:22 -0500

    implement raycast hit detection and configuration

--- addons/raycast_hitbox/plugin.gd
+++ addons/raycast_hitbox/plugin.gd
@@ -5,12 +5,14 @@ extends EditorPlugin
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	# Add the new type with a name, a parent type, a script and an icon.
-	pass
-	# add_custom_type("MyButton", "Button", \ preload("res://addons/raycast_hitbox/src/gdscript/raycast_hit_detector.gd"), preload("icon.png"))
+	add_custom_type("RayCastHitDetector", "Node3D",  \
+	preload("res://addons/raycast_hitbox/src/script/raycast_hit_detector.gd"), preload("res://addons/raycast_hitbox/src/assets/raycast_hit_detector_icon.png"))
 
+	add_custom_type("RayCastHitPoint", "Node3D",  \
+	preload("res://addons/raycast_hitbox/src/script/raycast_hit_detector.gd"), preload("res://addons/raycast_hitbox/src/assets/raycast_hit_point_icon.png"))
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
 	# Always remember to remove it from the engine when deactivated.
-	# remove_custom_type("MyButton")
-	pass
+	remove_custom_type("RayCastHitDetector")
+	remove_custom_type("RayCastHitPoint")
--- addons/raycast_hitbox/src/demo/controller.gd
+++ addons/raycast_hitbox/src/demo/controller.gd
@@ -7,21 +7,32 @@ extends CharacterBody3D
 
 var target_velocity = Vector3.ZERO
 
+var camera: Node3D
+
+func _on_hit(result: Dictionary):
+	print_debug("hit!")
+
+func _ready():
+	camera = $OverShoulderCamera
+	var detector: RayCastHitDetector = $StaticBody3D/Orb/RayCastHitDetector
+	detector.hit.connect(_on_hit)
+	detector.add_exclusion(self)
+	detector.begin()
+
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 
-	# Ground Velocity
-	target_velocity.x = Input.get_axis("move_right", "move_left") * speed
-	target_velocity.z = -Input.get_axis("move_forward", "move_backward") * speed
-	
+	var h_axis = Input.get_axis("move_left", "move_right")
+	var y_axis = Input.get_axis("move_forward", "move_backward") 
+	direction =	(camera.basis * -Vector3(h_axis, 0, y_axis)).normalized()
+		
+	velocity = direction * speed
 	
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+		velocity.y = velocity.y - (fall_acceleration * delta)
 
-	# Moving the Character
-	velocity = target_velocity
 	move_and_slide()
